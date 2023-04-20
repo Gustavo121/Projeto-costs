e87b7d9e574a05f434cf70e00ffe8b942dff35b0
Gustavo <gustgouvea@hotmail.com>
2023-04-20 12:04:34 -0300

    revisar

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -10,11 +10,13 @@ import Container from "../layout/Container";
 import Message from "../layout/Message";
 import ProjectForm from "../project/ProjectForm";
 import ServiceForm from "../service/ServiceForm";
+import ServiceCard from "../service/ServiceCard";
 
 function Project() {
   const { id } = useParams();
 
   const [project, setProject] = useState([]);
+  const [services, setServices] = useState([]);
   const [showProjectForm, setShowProjectForm] = useState(false);
   const [showServiceForm, setShowServiceForm] = useState(false);
   const [message, setMessage] = useState();
@@ -31,13 +33,14 @@ function Project() {
         .then((resp) => resp.json())
         .then((data) => {
           setProject(data);
+          setServices(data.services)
         })
         .catch((err) => console.log(err));
     }, 300);
   }, [id]);
 
   function editPost(project) {
-    setMessage('');
+    setMessage("");
 
     // budget validation
     if (project.budget < project.cost) {
@@ -64,10 +67,10 @@ function Project() {
   }
 
   function createService(project) {
-    setMessage('')
+    setMessage("");
 
     // last service
-    const lastService = project.services[project.services.length - 1];
+    const lastService = project.services[project.services.length - 1]
 
     lastService.id = uuidv4();
 
@@ -84,21 +87,48 @@ function Project() {
     }
 
     // add service cost to project total cost
-    project.cost = newCost
+    project.cost = newCost;
 
     // updade project
     fetch(`http://localhost:5000/projects/${project.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(project),
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        setShowServiceForm(false)
+      })
+      .catch((err) => console.log(err));
+  }
+
+  function removeService(id, cost) {
+
+    const servicesUpdated = project.services.filter(
+      (service) => service.id !== id
+    )
+
+    const projectUpdated = project
+
+    projectUpdated.services = servicesUpdated
+    projectUpdated.cost = parseFloat(projectUpdated.cost) - parseFloat(cost)
+
+    fetch(`http://localhost:5000/projects/${projectUpdated.id}`, {
       method: 'PATCH',
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify(project)
+      body: JSON.stringify(projectUpdated)
     }).then((resp) => resp.json())
     .then((data) => {
-      // exibir os serviços
-      console.log(data)
+      setProject(projectUpdated)
+      setServices(servicesUpdated)
+      setMessage('Serviço removido com sucesso!')
     })
     .catch(err => console.log(err))
+
   }
 
   function toggleProjectForm() {
@@ -159,7 +189,19 @@ function Project() {
             </div>
             <h2>Serviços</h2>
             <Container customClass="start">
-              <p>Itens de serviços</p>
+              {services.length > 0 &&
+                services.map((service) => (
+                  <ServiceCard
+                    id={service.id}
+                    name={service.name}
+                    cost={service.cost}
+                    description={service.description}
+                    key={service.key}
+                    handleRemove={removeService}
+                  />
+                ))
+              }
+              {services.length === 0 && <p>Não Há serviços cadastrados.</p>}
             </Container>
           </Container>
         </div>
