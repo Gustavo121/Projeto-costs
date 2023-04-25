f1fe3b68e9a5041234ca634f131630a9432ccb04
Gustavo <gustgouvea@hotmail.com>
2023-04-25 00:37:45 -0300

    erro novo corrigido

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -10,7 +10,7 @@ function NewProject() {
   function createPost(project) {
     // initialize cost and services
     project.cost = 0;
-    project.service = [0];
+    project.services = [0];
 
     fetch("http://localhost:5000/projects", {
       method: "POST",
@@ -32,7 +32,7 @@ function NewProject() {
   return (
     <div className={styles.newproject_container}>
       <h1>Criar Projeto </h1>
-      <p>Crie seu projeto para depois adcionar os serviços</p>
+      <p>Crie seu projeto para depois adicionar os serviços</p>
       <ProjectForm handleSubmit={createPost} btnText="Criar Projeto" />
     </div>
   );
